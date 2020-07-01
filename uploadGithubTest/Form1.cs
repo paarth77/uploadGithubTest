4aa4ef61ce18515da4cd2b17b4b1c2686bde8a90
Paarth <paarthadlakha@gmail.com>
2020-07-01 13:41:28 +1200

    changed

---
+++ uploadGithubTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uploadGithubTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "hello world";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
